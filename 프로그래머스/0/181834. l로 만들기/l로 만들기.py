8bc398055185d31142e57509abf778d035705118
hyunji <133614002+rmfpafls@users.noreply.github.com>
2024-10-01 23:52:42 +0900

    [level 0] Title: l로 만들기, Time: 9.14 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -0,0 +1,7 @@
+def solution(myString):
+    new_string = ''.join(['l' if char < 'l' else char for char in myString])
+    return new_string
+
+            
+                                        
+            
